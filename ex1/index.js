5c3c94d6d50d41c74c223efd8cba92f84004ccc4
IoanaDodi <ioanadodita1998@gmail.com>
2024-05-23 17:05:24 +0300

    am facut modificarile recomandate

--- ex1/index.js
+++ ex1/index.js
@@ -1,14 +1,14 @@
-function suma(numere) {
-    let suma = 0;
+function get_sum(numere) {
+    let get_sum = 0;
 
     for (let i = 0; i < numere.length; i++) {
-        suma += numere[i];
+        get_sum += numere[i];
     }
 
-    return suma;
+    return get_sum;
 }
 
 let numere = [3, 16, 98, 11, 6, 97];
 // let numere = [0.05, 48.69, -52, 153, -146.46]
-const total = suma(numere);
-console.log("Suma numerelor este: " + total );
+const total = get_sum(numere);
+console.log("Suma numerelor este: " + total);
